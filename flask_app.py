a50dbf38900fa29c63a02ddd83732e30d60acb1d
asselin <asselin@pheromone.ca>
2016-05-23 06:36:47 -0400

    update du serveur flask

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,49 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+import os.path
+
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+
+@app.route('/', methods=['GET'])
+def metrics():  # pragma: no cover
+    content = get_file('jenkins_analytics.html')
+    return Response(content, mimetype="text/html")
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def get_resource(path):  # pragma: no cover
+    mimetypes = {
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript",
+        ".jpg": "image/jpeg",
+        ".png": "image/png",
+        ".pdf": "application/pdf",
+    }
+    complete_path = os.path.join(root_dir(), path)
+    ext = os.path.splitext(path)[1]
+    mimetype = mimetypes.get(ext, "text/html")
+    content = get_file(complete_path)
+    return Response(content, mimetype=mimetype)
 
